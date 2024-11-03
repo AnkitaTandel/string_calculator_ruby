7e07eaffe16c3f5d0df1443908d0bf5d3c6f7e8c
Ankita <ankitaatandel@gmail.com>
2024-11-03 22:45:11 +0530

    handles single number string input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calculator.add('')).to eq 0
     end
+
+    it 'returns the number itself when a single number is provided' do
+      expect(calculator.add('1')).to eq(1)
+      expect(calculator.add('15')).to eq(15)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,5 +4,7 @@ class StringCalculator
 
   def add(string)
     return 0 if string.empty?
+
+    string.to_i
   end
 end
